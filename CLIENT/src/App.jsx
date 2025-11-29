18e4b18398aa894fc307e8fd95a931794a655194
Harshvardhan9560 <harshvardhan.singh.work09@gmail.com>
2025-11-29 21:21:07 +0530

    grow change

--- CLIENT/src/App.jsx
+++ CLIENT/src/App.jsx
@@ -111,7 +111,7 @@ function App() {
                 {
                   title: "Grow",
                   description:
-                    "This will be some random text that will be showcased here later after thoughtful consideration",
+                    "From curiosity to confidence, from learning to leading — we will help you grow into thinkers, creators, and changemakers in the world of technology",
                 },
               ].map((item, index) => (
                 <Card key={index} className="bg-slate-800 border-slate-700 p-8 text-center">
